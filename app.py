2914d9bd0334b5a2f1b9e5bd4981f45a308e430e
Gaurav-otisco <Kumar.Gaurav@otisco.se>
2023-05-01 11:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ import numpy as np
 import altair as alt
 import pydeck as pdk
 
+from annotated_text import annotated_text
+
 # SETTING PAGE CONFIG TO WIDE MODE
 st.set_page_config(layout="wide")
 
@@ -34,7 +36,7 @@ Has a good understanding of SQL and also has good Data Visualisation skills,
 and has worked with tools like Tableau and Python libraries such as Numpy and Pandas,
 Scikit learn, Matplotlib and Seaborn. 
 I'm highly passionate about Data Science and intending to pursue a career in Big data/Data Scientist. 
-github link - https://github.com/pramod1998-hash
+github
      ''')
 
 model = joblib.load(open('modelf.pkl', 'rb'))
@@ -46,18 +48,27 @@ st.write("**Summary Submitted**:\n", txt)
 int_features = [txt]
 #st.write(int_features)
 docs=list(model.pipe(int_features))
-lst=[]        
-for doc in docs:        
-    for entity in doc.ents:
-        #st.write((entity.text,entity.label_))
+lst=[]       
+annot_lst = [] 
+for doc in docs:   
+    prev_Start = 0     
+    for entity in doc.ents:     
+        annot_lst.extend([str(doc[prev_Start:entity.start])])
+        annot_lst.append((entity.text,entity.label_))
+
         lst.append((entity.text,entity.label_))
+        prev_Start = entity.end
+    annot_lst.extend([str(doc[prev_Start:])])
 
 output = lst
+
+
 
 df = pd.DataFrame(
     lst)
 
 st.write("### OUTPUT:-")
+annotated_text(annot_lst)
 st.dataframe(df)
 
 st.write("*Note: So far our AI has been train to extract only skill and experience. This AI can be trained further for extraction required information* ")
